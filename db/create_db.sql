2c3a913a7a6706fd8a00506a29ef72b9d15d5943
Kaspars Dzerins <kadzer@gmail.com>
2018-08-11 11:42:09 +0100

    updated create_db.sql to reflect a more realistic db structure

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,17 +1,34 @@
+DROP TABLE IF EXISTS sessions;
 DROP TABLE IF EXISTS clases;
+DROP TABLE IF EXISTS instructors;
 DROP TABLE IF EXISTS members;
+
 
 CREATE TABLE members (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(255),
   last_name VARCHAR(255),
-  join_date DATE DEFAULT CURRENT DATE,
-  enabled INT NOT NULL DEFAULT 1
+  join_date DATE
+);
+
+CREATE TABLE instructors (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  profile VARCHAR(255)
 );
 
 CREATE TABLE classes (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  description VARCHAR(255),
-  member_id INT REFERENCES members(id) ON DELETE CASCADE
+  description VARCHAR(255)
+);
+
+CREATE TABLE sessions (
+  id SERIAL PRIMARY KEY,
+  session_time TIME,
+  class_id INT REFERENCES classes(id),
+  instructor_id INT REFERENCES instructors(id),
+  member_id INT REFERENCES members(id),
+  capacity INT
 );
